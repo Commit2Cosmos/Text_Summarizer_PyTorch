d2a94272b380058548218e8b0484d339ad52f394
Anton Belov <anton.belov.al7@gmail.com>
2024-07-03 14:15:51 +0100

    utility methods added

---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,36 @@
+import json
+import os
+from textSummarizer.loggin import logger
+from pathlib import Path
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from ensure import ensure_annotations
+
+
+@ensure_annotations
+def read_json(path_to_json: Path) -> ConfigBox:
+    try:
+        with open(path_to_json) as json_file:
+            content = json.load(json_file)
+            logger.info(f"json file {json_file} has been loaded")
+            return ConfigBox(content)
+
+    except BoxValueError:
+        raise ValueError("json file is empty")
+    except Exception as e:
+        raise e
+    
+
+@ensure_annotations
+def create_directories(list_of_paths: list, verbose = True):
+    for dir in list_of_paths:
+        os.makedirs(dir, exist_ok=True)
+        if verbose:
+            logger.info(f"directory at {dir} has been created")
+
+
+@ensure_annotations
+def get_size_of_file(file_path: Path):
+    size_in_kb = os.path.getsize(file_path)
+    return f"size of {file_path} is {size_in_kb}"
+
